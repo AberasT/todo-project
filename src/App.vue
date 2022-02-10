10f7063242ae07f074d89a996dd466851b6c2cc7
Álvaro Aberastain Marzorati <a.aberastain.marzorati@gmail.com>
2022-02-10 15:42:19 -0300

    Create project with vue CLI. Design App.vue root component. Create Task.vue component

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,37 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </div>
-  <router-view/>
+  <h1>{{title}}</h1>
+  <ul>
+    <li v-for="(task,index) in tasks" :key="index">{{task}}</li>
+  </ul>
+  <input type="text">
+  <button @click="addTask">Add</button>
+
 </template>
+
+<script>
+import Task from "./components/Task";
+
+export default {
+  name: 'App',
+  data() {
+    return {
+      title: 'To-Do List',
+      tasks: ['1','2','3']
+    }
+  },
+  components: {
+    Task
+  }
+}
+</script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: Helvetica;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
 }
 
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
 </style>
---
+++ src/components/Task.vue
@@ -0,0 +1,21 @@
+<template>
+
+
+
+</template>
+
+<script>
+import App from "../App.vue";
+
+export default {
+  name: 'Task',
+  data() {
+    return {
+      task: ' '
+    }
+  },
+  components: {
+    
+  }
+}
+</script>
